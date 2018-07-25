2a326e7672dff383838e7a34e2e54bb1668fa67f
Ignacio <imaraz@htomail.es>
2018-07-25 13:16:16 +0200

    Probando node JS

--- Pruebas/GetStarted.js
+++ Pruebas/GetStarted.js
@@ -3,7 +3,7 @@
 */ 
 
 let { Zilliqa } = require('zilliqa.js');
-
+var Promise = require('promise');
 //For local testing, use URL = 'http://localhost:4201'
 //To connect to the external network, use URL = 'https://api-scilla.zilliqa.com'
 
@@ -26,13 +26,13 @@ function callback (err, data) {
 
 // generate a private key and its public address. You can change these to point to your own wallet if you have. 
 let privateKey = zilliqa.util.generatePrivateKey();
-let address = zilliqa.util.getAddressFromPrivateKey(privateKey);
-console.log("Mi dirección: " + address);
+let miDireccion = zilliqa.util.getAddressFromPrivateKey(privateKey);
+console.log("Mi dirección: " + miDireccion);
 /* 
     APIs 
 */
 
-node.getBalance({ address: address }, callback);
+node.getBalance({ address: miDireccion }, callback);
 
 // transaction details
 let txnDetails = {
@@ -47,10 +47,48 @@ let txnDetails = {
 // sign the transaction using util methods
 let txn = zilliqa.util.createTransactionJson(privateKey, txnDetails);
 
+var enviada = false;
 // send the transaction to the node
-node.createTransaction(txn, callback);
 
+
+node.createTransaction(txn, function(err, data){
+    //enviada = !(err || data.error);
+    callback(err, data);
+    var txId = data.result;
+    node.getBalance({ address: miDireccion }, function (err, data) {
+        console.log("Hemos enviado la transacción.");
+        if (err || data.error) {
+            console.log('Error')
+        } else {
+            console.log(data.result)
+        }
+    });
+    node.getTransaction({ txHash: txId }, function(err, data) {
+        if (err || data.result.error || !data.result['ID']) {
+            console.log(err || data.result.error);
+        } else {
+            console.log("Info de la transacción.")
+            console.log(data.result)
+        }
+    });
+});
+
+/*
+var actualizarBalance = function(transactionEnviada){
+    transactionEnviada
+        .then(function(dirección){
+            console.log("Hemos enviado la transacción.")
+            node.getBalance({ address: direccion }, callback);
+        })
+        .catch(function(error){
+            console.log(error.message);
+        })
+};
+actualizarBalance();
+*/
 //Vemos de nuevo el balance
 setTimeout(function() { 
-    node.getBalance({ address: address }, callback); }, 
+    console.log("Segunda llamada al balance:")
+    node.getBalance({ address: miDireccion }, callback); }, 
     5000);
+
